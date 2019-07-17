49192db8b18df30a6e1d7b78f18076f9062a94a4
riteshgupta87993 <52970990+riteshgupta87993@users.noreply.github.com>
2019-07-17 14:51:11 +0530

    ngo all files

---
+++ index.html.php
@@ -0,0 +1,374 @@
+
+<html>
+<head>
+<title>NGO
+</title>
+
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<style>
+*{
+	margin:0px;
+	padding:0px;
+}
+#page{
+	height:1500px;
+	width:1500px;
+	background-color:skyblue;
+	
+	}
+	#header{
+		
+	height:60px;
+	width:1500px;
+	background-color:red;
+	
+	}
+	#head1{
+	height:60px;
+	width:850px;
+	background-color:orange;
+	float:left;
+	
+	}
+	#head2{
+	height:60px;
+	width:650px;
+	background-color:pink;
+	float:right;
+		
+	}
+	#menu{
+	height:40px;
+	width:1500px;
+	background-color:green;	
+	}
+	#menu ul {
+		list-style:none;
+		
+		
+	}
+	#menu ul li{
+		background-color:green;
+		height:40px;
+		border:1px  white;
+		width:150px;
+		float:left;
+		text-align:center;	
+		line-height:40px;
+		position:relative;
+		
+	}
+#menu ul li a{
+	text-decoration:none;
+	color:white;
+	display:block;
+	margin-left:0px;
+	
+}
+#menu ul li a:hover{
+background-color:red;
+}	
+#menu ul ul{
+	position:absolute;
+	display:none;
+}
+#menu ul li:hover > ul{
+	display:block;
+}
+#head2 ul li{
+	text-align:center;
+	
+	width:150px;
+	line-height:60px;
+	float:left;
+
+}
+#head2 ul li a{
+		text-decoration:none;
+		color:purple;
+}
+#head2 ul{
+	list-style:none;
+}
+#head1{
+	line-height:60px;
+}
+#image{
+	height:400px;
+	width:1500px;
+	background-color:yellow;
+}
+
+	#slide1{	
+		height:399px;
+		width:373px;
+		float:left;
+		background-image:url('images.jpg');
+		background-repeat : no-repeat;
+	}
+	#slide2{
+		
+		height:399px;
+		width:373px;
+		background-image:url('pc.jpg');
+		background-repeat : no-repeat;
+		float:left;
+		
+	}
+	#slide3{
+		
+		height:399px;
+		width:373px;
+		background-image:url('pollution.jpg');
+		background-repeat : no-repeat;
+		float:left;
+	}
+	#slide4{
+		
+		height:399px;
+		width:373px;
+		background-image:url('save nature.jpg');
+		background-repeat : no-repeat;
+		float:left;
+	}
+
+#projects{
+	
+	height:300px;
+	width:1500px;
+	background-color:pink;
+	
+}
+
+#projects1{
+	
+	height:300px;
+	width:350px;
+	background-color:orange;
+	
+	float:left;
+}
+
+#projects2{
+	
+	height:300px;
+	width:350px;
+	background-color:pink;
+	float:left;
+}
+
+#projects3{
+	
+	height:300px;
+	width:350px;
+	background-color:purple;
+	
+	float:left;
+}
+
+#projects4{
+	
+	height:300px;
+	width:350px;
+	background-color:blue;
+	
+	float:left;
+}
+#about{
+	
+	height:400px;
+	width:1500px;
+	background-color:red;
+	
+}
+
+#about1{
+	
+	height:400px;
+	width:500px;
+	background-color:blue;
+	float:left;
+}
+#about2{
+	
+	height:400px;
+	width:490px;
+	background-color:purple;
+	float:left;
+}
+#about3{
+	
+	height:400px;
+	width:500px;
+	background-color:orange;
+	
+}
+#footer{
+	height:280px;
+	width:1500px;
+	background-color:grey;
+}	
+
+#footer4{
+	height:280px;
+	width:499px;
+	background-color:grey;
+	float:left;
+	
+	}	
+#footer2{
+	height:280px;
+	width:499px;
+	background-color:grey;
+	float:left;
+	}	
+#footer3{
+	height:280px;
+	width:499px;
+	background-color:grey;
+	float:left;
+	
+	}	
+#footer5{
+	height:20px;
+	width:1500px;
+	background-color:grey;
+}	
+</style>
+</head>
+<body>
+<div id=page>
+		<div id=header>
+			<div id=head1>
+			<center><marquee><strong><h1><font color=red >NON GOVERMENTAL ORGANIZATION</font></h1></strong>
+			</marquee></center></div>
+			<div id=head2>
+				<ul>
+					<li><h4><a href='home.html'>HOME</a></h4></li>
+					<li></h4><a href='contact.html'><b>CONTACT US</b></a></h4></li> 
+			
+				</ul>
+			</div>
+		</div>
+		<div id=menu>
+		
+			<ul>
+				<li><a href=''>About Us </a>
+					
+					<ul>
+						<li><a href=''>Introduction</a></li>
+						<li><a href=''>Aims and objective</a></li>
+						<li><a href=''>Board of governers</a></li>
+					</ul>
+				</li>
+				<li><a href=''>Our Work</a>
+					
+					<ul>
+						<li><a href=''>projects</a></li>
+						<li><a href='sa.html'>spritual activities</a></li>
+						
+						<li><a href='tg.html'>tribal and girl education</a></li>
+						<li><a href='mh.html'>medical health</a></li>
+					</ul>
+				</li>
+				<li><a href=''>Get Involved</a>
+					
+					<ul>
+						<li><a href=''>Donate</a></li>
+						<li><a href=''>Campous participation</a></li>
+					
+					</ul>
+				</li>
+				<li><a href=''>Media</a>
+					
+					<ul>
+						<li><a href=''>vgcf brochure</a></li>
+						<li><a href=''>press release</a></li>
+						<li><a href=''>photo gallary</a></li>
+						<li><a href=''>video clips</a></li>
+					</ul>
+				</li>
+				<li><a href=''>Search</a></li>
+				<li><a href=''>Events/Activities</a></li>
+			</ul>
+		</div>
+<div id=image>		
+	
+		<div id=slide1>	</div>
+		<div id=slide2></div>
+		<div id=slide3></div>
+		<div id=slide4></div>
+
+	
+</div>
+				<div id=projects >
+					
+					<div id=projects1>
+					<h1>OUR PROJECTS</h1>
+					</div>
+					
+					<div id=projects2>
+					<h1>PROJECTS1</h1>
+					</div>
+					
+					<div id=projects3>
+					<h1> PROJECT2</h1>
+					</div>
+					
+					<div id=projects4>
+					<h1>PROJECT3</h1>
+					</div>
+				</div>
+			
+			<div id=about>
+				
+					<div id=about1>
+					<h1>ABOUT YOURSELF</h1>
+					<p>'<h2>my name is ritesh gupta .
+					i'm from lucknow.
+					i've completed my btech with 73% computer science branch from a.k.t.u university. 
+					</h2>
+					</p>
+					</div>
+					
+					<div id=about2>
+					</div>
+					
+					<div id=about3>
+					</div>
+			</div>
+	<div id=footer>
+			
+		<div id=footer2>
+		<p>
+		<center><font color=red><h1>ritesh gupta</h1></red>	</center>
+		
+		<center><font color=red><h1>web developer</h1></red></center>
+		</p>
+		</div>		
+		<div id=footer3>
+		<p>
+		<ul><h1>Importants links</h1>
+			<h3><li><a href=>videos</a></li></h3>
+			<h3><li><a href=>links</a></li></h3>
+		</ul>
+		</p>
+		</div>		
+		<div id=footer4>
+		<p>
+		<ul><h1>CONTACT </h1>
+			<li>nihal vihar nangloi new delhi 110041</li>
+			<li>8799329020</li>
+			<li>6387664828</li>
+		</ul>
+		</p>
+		</div>		
+	</div>		
+	
+	<div id=footer5>
+		<center><h4>EMAIL-ID=RITESHGUPTA@GMAIL.COM</h4></center>
+	</div>		
+</div>
+</body>
+</html>
